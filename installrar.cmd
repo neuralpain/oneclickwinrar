619ad9a9275a1ffbb1566535a27c79600b665982
neuralpain <77242216+neuralpain@users.noreply.github.com>
2024-08-01 16:40:44 +0800

    feature: enable custom download
    Allow users to download a specific version of WinRAR by modifying the script's file name

--- installrar.cmd
+++ installrar.cmd
@@ -13,7 +13,7 @@ setlocal EnableExtensions DisableDelayedExpansion
 set ARGS=%*
 if defined ARGS set ARGS=%ARGS:"=\"%
 if defined ARGS set ARGS=%ARGS:'=''%
-PowerShell -NoP -C ^"Invoke-Expression ('^& {' + (get-content -raw '%~f0') + '} %ARGS%')"
+PowerShell -NoP -C ^"$CMD_NAME='%~n0';Invoke-Expression ('^& {' + (get-content -raw '%~f0') + '} %ARGS%')"
 exit /b
 
 # --- PS --- #>
@@ -23,22 +23,51 @@ $global:ProgressPreference = "SilentlyContinue"
 
 $Script:WINRAR_EXE = $null
 $Script:FETCH_WINRAR = $false
+$Script:CUSTOM_DOWNLOAD = $false
 $winrar = "winrar-x\d{2}-\d{3}\w*\.exe" # catch any version for any language
+
+$Script:ARCH = $null # download architecture
+$Script:RARVER = $null # download version
+$Script:TAGS = $null # other download types, eg. language, beta, etc.
+
+function New-Toast {
+  [CmdletBinding()]Param ([String]$ToastTitle, [String][parameter(ValueFromPipeline)]$ToastText)
+  [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
+  $Template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText02)
+  $RawXml = [xml] $Template.GetXml(); ($RawXml.toast.visual.binding.text | Where-Object { $_.id -eq "1" }).AppendChild($RawXml.CreateTextNode($ToastTitle)) > $null; ($RawXml.toast.visual.binding.text | Where-Object { $_.id -eq "2" }).AppendChild($RawXml.CreateTextNode($ToastText)) > $null
+  $SerializedXml = New-Object Windows.Data.Xml.Dom.XmlDocument; $SerializedXml.LoadXml($RawXml.OuterXml);
+  $Toast = [Windows.UI.Notifications.ToastNotification]::new($SerializedXml); $Toast.Tag = "PowerShell"; $Toast.Group = "PowerShell"; $Toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(1)
+  $Notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("PowerShell"); $Notifier.Show($Toast);
+}
+
+function Get-WinRARData {  
+  $Script:CUSTOM_DOWNLOAD = $true
+  $_data = [regex]::matches($CMD_NAME, '[^_]+')
+  # `$_data[0]` is the script name
+  $Script:ARCH = $_data[1].Value
+  $Script:RARVER = $_data[2].Value
+  $Script:TAGS = $_data[3].Value
+  
+  if ($Script:ARCH.Length -ne 3 -or $Script:RARVER.Length -ne 3) {
+    New-Toast -ToastTitle "oneclickwinrar: Error" -ToastText "Download data is invalid."; exit
+  }
+  if ($Script:ARCH -ne "x64" -and $Script:ARCH -ne "x86") {
+    New-Toast -ToastTitle "oneclickwinrar: Error" -ToastText "Download architecture is invalid."; exit
+  }
+}
 
 function Invoke-Installer($file) {
   $x = if ($file -match "(?<version>\d{3})") { ($matches['version']) / 100 } # get WinRAR version number
   Write-Host "Installing WinRAR v${x}..."
-  
   try {
     Start-Process $file "/s" -Wait
-    Write-Host "WinRAR installed successfully." -ForegroundColor Green
   }
   catch {
-    Write-Host "Installer ran into a problem." -ForegroundColor Red
-    Write-Host "Please restart the script." -ForegroundColor Yellow
-    Pause; exit
+    New-Toast -ToastTitle "oneclickwinrar: Installation Error" -ToastText "Please restart the script."; exit
   }
-  finally { if ($Script:FETCH_WINRAR) { Remove-Item $Script:WINRAR_EXE } }
+  finally {
+    if ($Script:FETCH_WINRAR) { Remove-Item $Script:WINRAR_EXE }
+  }
 }
 
 function Get-Installer {
@@ -46,24 +75,29 @@ function Get-Installer {
   foreach ($file in $files) { if ($file -match $winrar) { return $file } }
 }
 
-$Script:WINRAR_EXE = (Get-Installer)
+if ($CMD_NAME -ne "installrar") { Get-WinRARData }
 
+$Script:WINRAR_EXE = (Get-Installer)
 if ($null -eq $Script:WINRAR_EXE) {
+  if ($Script:CUSTOM_DOWNLOAD) { Get-CustomDownload }
   Write-Host "Testing connection... " -NoNewLine
   if (Test-Connection www.google.com -Quiet) {
     Write-Host -NoNewLine "OK.`nDownloading WinRAR... "
-    try { Start-BitsTransfer "https://www.rarlab.com/rar/winrar-x64-701.exe" $pwd\ }
+    try {
+      if ($Script:CUSTOM_DOWNLOAD) { Start-BitsTransfer "https://www.rarlab.com/rar/winrar-${Script:ARCH}-${Script:RARVER}${Script:TAGS}.exe" $pwd\ }
+      else { Start-BitsTransfer "https://www.rarlab.com/rar/winrar-x64-701.exe" $pwd\ }
+    }
     catch {
-      Write-Host "`nDownloader ran into a problem." -ForegroundColor Red
-      Write-Host "Please check your internet connection." -ForegroundColor Yellow
-      Pause; exit
+      New-Toast -ToastTitle "oneclickwinrar: Download Error" -ToastText "Please check your internet connection."; exit
     } 
     $Script:WINRAR_EXE = (Get-Installer)
     $Script:FETCH_WINRAR = $true
     Write-Host "Done."
-  } else { Write-Host "No internet."; Pause; exit }
+  }
+  else { 
+    New-Toast -ToastTitle "oneclickwinrar: Download Error" -ToastText "Please check your internet connection."; exit
+  }
 }
 
 Invoke-Installer $Script:WINRAR_EXE
-Start-Sleep -Seconds 2
-exit
+New-Toast -ToastTitle "oneclickwinrar" -ToastText "WinRAR installed successfully."; exit
