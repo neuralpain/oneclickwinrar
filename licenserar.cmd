a7afdbca9b53f5c0446f05d30477992852aceb65
neuralpain <77242216+neuralpain@users.noreply.github.com>
2024-08-04 14:05:21 +0800

    feat: allow users to overwrite existing licenses

--- licenserar.cmd
+++ licenserar.cmd
@@ -39,10 +39,10 @@ exit /b
 # --- PS --- #>
 
 $script_name = "licenserar"
-# $script_name_overwrite = "license-rar"
+$script_name_overwrite = "license-rar"
 
-$Script:CUSTOM_LICENSE = $false;
-# $OVERWRITE_LICENSE = $false;
+$Script:CUSTOM_LICENSE = $false
+$OVERWRITE_LICENSE = $false
 
 $rarkey = "RAR registration data`r`nEveryone`r`nGeneral Public License`r`nUID=119fdd47b4dbe9a41555`r`n6412212250155514920287d3b1cc8d9e41dfd22b78aaace2ba4386`r`n9152c1ac6639addbb73c60800b745269020dd21becbc46390d7cee`r`ncce48183d6d73d5e42e4605ab530f6edf8629596821ca042db83dd`r`n68035141fb21e5da4dcaf7bf57494e5455608abc8a9916ffd8e23d`r`n0a68ab79088aa7d5d5c2a0add4c9b3c27255740277f6edf8629596`r`n821ca04340a7c91e88b14ba087e0bfb04b57824193d842e660c419`r`nb8af4562cb13609a2ca469bf36fb8da2eda6f5e978bf1205660302"
 $rarreg64 = "$env:ProgramFiles\WinRAR\rarreg.key"
@@ -71,23 +71,47 @@ function New-Toast {
 
 # check for custom license
 if ($CMD_NAME -ne $script_name) {
-  $Script:CUSTOM_LICENSE = $true
   $_data = [regex]::matches($CMD_NAME, '[^_]+')
-  $Script:LICENSEE = $_data[0].Value
-  $Script:LICENSE_TYPE = $_data[1].Value
-  # `$_data[2]` is the script name
-  
-  # if ($_data[2].Value -eq $script_name_overwrite) {
-  #   $OVERWRITE_LICENSE = $true
-  # }
-  if ($_data[2].Value -ne $script_name) { # -and $_data[2].Value -ne $script_name_overwrite) {
-    New-Toast -ToastTitle "oneclickwinrar: Error" -ToastText "Script name is invalid."; exit
+  if ($_data.Count -eq 1) {
+    # user only wants to overwrite the existing license
+    if ($_data[0].Value -eq $script_name_overwrite) {
+      $Script:OVERWRITE_LICENSE = $true
+    }
+    else {
+      New-Toast -ToastTitle "oneclickwinrar: Error" -ToastText "Script name is invalid."; exit
+    }
   }
-  if ($Script:LICENSEE.Length -eq 0 -or $Script:LICENSE_TYPE.Length -eq 0) {
-    New-Toast -ToastTitle "oneclickwinrar: License Error" -ToastText "Custom lincense data is invalid."; exit
+  else {
+    # `$_data[2]` is the script name
+    $Script:CUSTOM_LICENSE = $true
+    switch ($_data[2].Value) {
+      $script_name {
+        # custom license, script name is valid
+        $Script:LICENSEE = $_data[0].Value
+        $Script:LICENSE_TYPE = $_data[1].Value
+      }
+      $script_name_overwrite {
+        # custom license, script name is valid, but user
+        # wants to overwrite an existing license
+        $OVERWRITE_LICENSE = $true
+        $Script:LICENSEE = $_data[0].Value
+        $Script:LICENSE_TYPE = $_data[1].Value
+      }
+      default {
+        # custom license, but script name is invalid
+        New-Toast -ToastTitle "oneclickwinrar: License Error" -ToastText "Custom lincense data is invalid."; exit
+      }
+    }
+    # verify custom license data --- this is a sanity check
+    # the script should not reach this point, but I'm leaving
+    # it here as a precaution just in case I missed something
+    if ($Script:LICENSEE.Length -eq 0 -or $Script:LICENSE_TYPE.Length -eq 0) {  
+      New-Toast -ToastTitle "oneclickwinrar: License Error" -ToastText "Custom lincense data is invalid."; exit
+    }
   }
 }
 
+# check for WinRAR architecture
 if (Test-Path $winrar64 -PathType Leaf) {
   $keygen = $keygen64
   $rarreg = $rarreg64
@@ -97,13 +121,14 @@ elseif (Test-Path $winrar32 -PathType Leaf) {
   $rarreg = $rarreg32
 }
 else {
-  New-Toast -ToastTitle "oneclickwinrar: Error" -ToastText "WinRAR is not installed. Please run installrar.cmd or oneclickrar.cmd to install WinRAR."; exit 
+  New-Toast -ToastTitle "oneclickwinrar: Error" -ToastText "WinRAR is not installed. Please run installrar.cmd or oneclickrar.cmd to install WinRAR."; exit
 }
 
-if (-not(Test-Path $rarreg -PathType Leaf)) { 
+# install WinRAR license
+if (-not(Test-Path $rarreg -PathType Leaf) -or $OVERWRITE_LICENSE) {
   if ($Script:CUSTOM_LICENSE) {
     if (Test-Path $keygen -PathType Leaf) {
-      & $keygen64 "$($Script:LICENSEE)" "$($Script:LICENSE_TYPE)" | Out-File -Encoding utf8 $rarreg
+      & $keygen "$($Script:LICENSEE)" "$($Script:LICENSE_TYPE)" | Out-File -Encoding utf8 $rarreg
     }
     else {
       New-Toast -ToastTitle "oneclickwinrar: Missing keygen" -ToastText "Unable to generate license."; exit
@@ -117,8 +142,9 @@ if (-not(Test-Path $rarreg -PathType Leaf)) {
       [IO.File]::WriteAllLines($rarreg, $rarkey)
     }
   }
-} else {
-  New-Toast -ToastTitle "oneclickwinrar: Error" -ToastText "WinRAR license already exists."; exit
+}
+else {
+  New-Toast -ToastTitle "oneclickwinrar: Error" -ToastText "A WinRAR license already exists."; exit
 }
 
 New-Toast -ToastTitle "oneclickwinrar" -ToastText "WinRAR licensed successfully."; exit
