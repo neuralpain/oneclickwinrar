57551aff7b6062a9c5b77fda5689c56009b8cfec
neuralpain <77242216+neuralpain@users.noreply.github.com>
2024-12-21 11:49:26 +0800

    Update project version
    0.7.0.701 -> 0.8.0.701

--- oneclickrar.cmd
+++ oneclickrar.cmd
@@ -1,13 +1,13 @@
 <# :# DO NOT REMOVE THIS LINE
 
 :: oneclickrar.cmd
-:: oneclickwinrar, version 0.7.0.701
+:: oneclickwinrar, version 0.8.0.701
 :: Copyright (c) 2023, neuralpain
 :: Install and license WinRAR
 
 @echo off
 mode 44,8
-title oneclickrar (v0.7.0.701)
+title oneclickrar (v0.8.0.701)
 :: uses PwshBatch.cmd <https://gist.github.com/neuralpain/4ca8a6c9aca4f0a1af2440f474e92d05>
 setlocal EnableExtensions DisableDelayedExpansion
 set ARGS=%*
