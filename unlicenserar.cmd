123372461378bad71c90cb31db7a9165ba0bf127
neuralpain <77242216+neuralpain@users.noreply.github.com>
2025-03-28 21:40:14 +0800

    chore: update project version
    0.10.0.710 -> 0.10.0.711

--- unlicenserar.cmd
+++ unlicenserar.cmd
@@ -1,13 +1,13 @@
 <# :# DO NOT REMOVE THIS LINE
 
 :: unlicenserar.cmd
-:: oneclickwinrar, version 0.9.0.701
+:: Last updated @ v0.9.0.701
 :: Copyright (c) 2023, neuralpain
 :: Un-license WinRAR
 
 @echo off
 mode 44,8
-title unlicenserar (v0.10.0.710)
+title unlicenserar (v0.10.0.711)
 :: uses PwshBatch.cmd <https://gist.github.com/neuralpain/4ca8a6c9aca4f0a1af2440f474e92d05>
 setlocal EnableExtensions DisableDelayedExpansion
 set ARGS=%*
