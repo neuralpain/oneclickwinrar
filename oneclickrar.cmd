a44a5eb1f1b722d5a54f5b6250d09144481a00d6
neuralpain <77242216+neuralpain@users.noreply.github.com>
2025-06-03 21:33:13 +0800

    fix: architecture location assignment error
    Option 2 will now assign the 64-bit location.

--- oneclickrar.cmd
+++ oneclickrar.cmd
@@ -792,7 +792,7 @@ function Select-CurrentWinrarInstallation {
     do {
       $query = Read-Host "Enter `"1`" for 32-bit and `"2`" for 64-bit"
       if ($query -eq 1) { $script:WINRAR_INSTALLED_LOCATION = $loc32; break }
-      elseif ($query -eq 2) { $script:WINRAR_INSTALLED_LOCATION = $loc32; break }
+      elseif ($query -eq 2) { $script:WINRAR_INSTALLED_LOCATION = $loc64; break }
     } while ($true)
   }
   Write-Info "Selected WinRAR installation: $(Format-Text $($script:WINRAR_INSTALLED_LOCATION) -Foreground White -Formatting Underline)."
